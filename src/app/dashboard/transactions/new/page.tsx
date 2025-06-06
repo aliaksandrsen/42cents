c9572ce5e144fe7643c71a8679c6c58b484b1427
Alex <puskin1914@gmail.com>
2025-06-06 22:12:25 +0400

    create NewTransactionPage

---
+++ src/app/dashboard/transactions/new/page.tsx
@@ -0,0 +1,48 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+export default function NewTransactionPage() {
+  return (
+    <div className="max-w-screen-xl mx-auto p-10">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/transactions">Transactions</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>New Transaction</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <Card className="mt-4 max-w-screen-md">
+        <CardHeader>
+          <CardTitle className="text-xl">New Transaction</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>new transaction form</p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
